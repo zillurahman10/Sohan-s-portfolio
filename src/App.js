e340d88eb1d80d7ea22c56b464b5d2162b19f9f4
zilurahman10 <raihan.brac2019@gmail.com>
2022-06-15 15:22:37 +0600

    s

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Routes, Route } from 'react-router-dom'
 import Header from './Shared/Header';
 import Home from './Pages/Home/Home';
 import ProjectDetails from './Pages/ProjectDetails/ProjectDetails';
+import ParticleBackground from './particleBackground';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <ParticleBackground></ParticleBackground>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/project/:id' element={<ProjectDetails></ProjectDetails>}></Route>
